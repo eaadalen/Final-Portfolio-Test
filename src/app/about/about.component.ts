b732d269494fb4ecfeaa2447b53c08da88836ba8
eaadalen <eaadalen112@gmail.com>
2024-01-24 14:46:30 -0600

    Improvements

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goToHome(): void {
+    this.router.navigate(['welcome']);
+    console.log("here");
+  }
+
+  goToWork(): void {
+    this.router.navigate(['main-view']);
+    console.log("here");
+  }
+
+  goToContact(): void {
+    this.router.navigate(['contact']);
   }
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goToHome(): void {
+    this.router.navigate(['welcome']);
+    console.log("here");
+  }
+
+  goToWork(): void {
+    this.router.navigate(['main-view']);
+    console.log("here");
+  }
+
+  goToAbout(): void {
+    this.router.navigate(['about']);
+  }
+
+  sendMessage(): void {
+    
   }
 
 }
